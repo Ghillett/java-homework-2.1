a65a494477373d11b031d70be712ec73b184ba4d
JP <email>
2022-10-08 17:31:51 +1000

    initial commit

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1000;
+
+        int bonusCondition = 20;
+
+        int bonus = ticketPrice / bonusCondition;
+
+        System.out.println(bonus +" бонусных миль");
+    }
+}
